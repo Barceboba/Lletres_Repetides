b3a4bea94cf354f0696e120c1b24b41e0f3017ee
Barceboba <miquel@barceboba.com>
2021-05-27 19:31:47 +0200

    nivell3 funciona, pa pulir

--- js/nivell3.js
+++ js/nivell3.js
@@ -1,18 +1,12 @@
-let myWay = ['M', 'I', 'Q', 'U', 'E', 'L'];
-let conta = {};
-// looping through your array
-for (let i = 0; i < myWay.length; i++) {
-  let num = myWay[i];
-  // this is a ternary if statement increasing the count
-  // if name(number) exists in your object add 1, else add number with value of 1
-  conta[num] = conta[num] ? conta[num] + 1 :1;
-  /*
-  Could also be written like this
-  if (counts[num]) {
-      counts[num] += 1;
+let myWay = ['M', 'I', 'Q', 'U', 'U', 'E', 'L'];
+let works = {};
+
+let conta = myWay.map(men => {
+  if (!works[men]){
+   works[men]= 1;
   } else {
-      counts[num] = 1;
-  }
-  */
-}
-console.log(conta);
+    works[men]++;
+  } return works;
+ // return conta = conta ? conta + 1 :1;
+});
+    console.log(conta[0]);
